373b1bcd0facfd30148a44d4cf5ff2da8d5ffd80
Elena Tonini <e.tonini002@studenti.unibs.it>
2024-04-25 14:40:14 +0200

    refactoring

--- mi.py
+++ mi.py
@@ -4,10 +4,11 @@ import numpy as np
 from scipy.stats import norm
 from tqdm import tqdm
 
+MU_SIGMA_TXT = "mu_sigma.txt"
+
 
 def mi(df, path=""):
-    df = df - df.min().min()  # shift data so that it ranges from 0 to max
-    df = df / (df.max().max())  # normalize data so that it ranges from 0 to 1
+    df = normalize(df)
 
     if path != "" and not os.path.exists(path):
         os.mkdir(path)
@@ -24,8 +25,8 @@ def mi(df, path=""):
             increments[cur_row, :] = diff
             cur_row += 1
 
-    # fit normal distribution to the data to compute mean and sigma of the distribution of the increments
-    with open(os.path.join(path, "mu_sigma.txt"), "w") as f:
+    # fit normal distribution to increments to compute mean and sigma of the distribution
+    with open(os.path.join(path, MU_SIGMA_TXT), "w") as f:
         mu, sigma = norm.fit(increments)
         f.write("MU: " + str(mu) + "\tSIGMA: " + str(sigma) + "\n")
     f.close()
@@ -33,14 +34,33 @@ def mi(df, path=""):
     # for each delta_t compute increments between CSI at time t and CSI at time t + delta_t (for each t)
     # fit normal distribution to the data to compute mean and sigma of the distribution of the increments
     # with open(os.path.join(path, "mu_sigma_delta_t.txt"), "w") as f:
-    #     for delta in tqdm(range(1, rows), total=rows - 1):
-    #         diffs = []
-    #         for row in range(1, rows):
-    #             if row + delta >= math.floor(rows/2):
-    #                 break
-    #             diffs.extend(df.iloc[row, :] - df.iloc[row + delta, :])
-    #         mu, sigma = norm.fit(diffs)
-    #         f.write("DELTA_T: " + str(delta) + "\tMU: " + str(mu) + "\tSIGMA: " + str(sigma) + "\n")
+    # for delta in tqdm(range(1, rows), total=math.floor(rows / 2)):
+    #     diffs = []
+    #     for row in range(1, rows):
+    #         if row + delta >= math.floor(rows / 2):
+    #             break
+    #         diffs.extend(df.iloc[row, :] - df.iloc[row + delta, :])
+    #     mu, sigma = norm.fit(diffs)
+    #     f.write("DELTA_T: " + str(delta) + "\tMU: " + str(mu) + "\tSIGMA: " + str(sigma) + "\n")
     # f.close()
 
-    return (df * 255).astype(int)  # quantize over 256 levels (0 to 255)
+    return quantize(df, num_intervals=255)  # quantize over 256 levels (0 to 255)
+
+
+def normalize(df):
+    """Normalize data so that it ranges from 0 to 1.
+    :param df: dataframe to normalize
+    :return: normalized dataframe
+    """
+    df = df - df.min().min()  # shift data so that it ranges from 0 to max
+    df = df / (df.max().max())  # normalize data so that it ranges from 0 to 1
+    return df
+
+
+def quantize(df, num_intervals):
+    """Quantize data into num_intervals levels.
+    :param df: dataframe to quantize
+    :param num_intervals: number of intervals
+    :return: quantized dataframe
+    """
+    return (df * num_intervals).astype(int)
