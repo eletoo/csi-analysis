450b5006e0c15584379e251502fcb4da67e148bf
Elena Tonini <e.tonini002@studenti.unibs.it>
2024-06-18 15:19:49 +0200

    A_c VS A_c^q plot
    Signed-off-by: Elena Tonini <e.tonini002@studenti.unibs.it>

--- plotcsi.py
+++ plotcsi.py
@@ -21,7 +21,7 @@ def plotcsi(df, n=1, title='CSI Amplitude', xlabel='Subcarrier index', ylabel='A
     plt.show()
 
 
-def plotcsi_quant(df, df_quant, n=1, q_amp=8, title='CSI Amplitude', xlabel='Subcarrier index', ylabel='Amplitude',
+def plotcsi_quant(df, df_quant, n=1, q_amp=8, title='CSI Amplitude', xlabel=r'Subcarrier index ($n$)', ylabel=r'$A_c$',
                   path=''):
     # undo quantization
     a = 0
@@ -29,20 +29,29 @@ def plotcsi_quant(df, df_quant, n=1, q_amp=8, title='CSI Amplitude', xlabel='Sub
     dequant = (df_quant - a) * (b - a)
     dequant = mi.normalize(dequant)
 
+    dfnorm = mi.normalize(df)
+
     # plt.figure()
     fig, ax = plt.subplots(3, 1, sharex=True)
     for i in range(n):
         idx = np.random.randint(0, len(df))
-        ax[0].plot(df.iloc[idx], label='A_c ' + str(idx))
-        ax[1].plot(df_quant.iloc[idx], label='A_c^q ' + str(idx))
-        ax[2].plot(df.iloc[idx] - dequant.iloc[idx], label='A_c - A_c^q ' + str(idx))
+        ax[0].plot(dfnorm.iloc[idx], label=r'$A({' + str(idx) + '},n)$')
+        ax[1].plot(df_quant.iloc[idx], label=r'$A({' + str(idx) + '},n)^q$')
+        ax[2].plot(dfnorm.iloc[idx] - dequant.iloc[idx], label=r'$A({' + str(idx) + '},n)-A({' + str(idx) + '},n)^q$')
     ax[0].legend(loc='upper right', fontsize=5)
+
     fig.suptitle(title)
+
     plt.xlabel(xlabel)
     xticks(np.arange(0, len(df.columns), 25), fontsize=6)
+
+    ax[0].set_ylim(0, 1)
     ax[0].set_ylabel(ylabel)
-    ax[1].set_ylabel('Quantized ' + ylabel)
-    ax[2].set_ylabel('Difference')
+    ax[1].set_ylim(0, 2 ** q_amp)
+    ax[1].set_ylabel(r'$A_c^q$')
+    ax[2].set_ylim(-0.1, 0.1)
+    ax[2].set_ylabel(r'$A_c - A_c^q$')
+
     ax[0].grid()
     ax[1].grid()
     ax[2].grid()
