3478c3f49e3fcb63aa0b70fbb4ad0d540220c01b
Elena Tonini <e.tonini002@studenti.unibs.it>
2024-07-09 09:34:14 +0200

    computing internal mutual information
    Signed-off-by: Elena Tonini <e.tonini002@studenti.unibs.it>

--- mi.py
+++ mi.py
@@ -1,137 +1,70 @@
-import math
-import os
-
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.stats import norm
-
-MEAN_CSI_CSV = "mean_CSI.csv"
-
-MU_SIGMA_TXT = "mu_sigma.txt"
-QB = 4  # qb = 3, 4, 5 to have support over 7, 15, 31 levels
+from decimal import Decimal
 
 
-def mi(df, qb=QB, path=""):
-    if path != "" and not os.path.exists(path):
-        os.mkdir(path)
-
-    df = normalize(df)
-
-    # rows, cols = df.shape
-    # increments = np.nan * np.ones((int(rows * (rows - 1) / 2), cols))
-
-    # # compute increments between CSI at time t and CSI at time t + delta_t (for each t and for each delta_t)
-    # cur_row = 0
-    # for i in tqdm(range(rows), total=rows):
-    #     row = df.iloc[i, :]
-    #     for j in range(i + 1, rows):
-    #         diff = (row - df.iloc[j, :]).values
-    #         increments[cur_row, :] = diff
-    #         cur_row += 1
-
-    increments = df.diff().dropna()  # DEBUG - compute increments between CSI at time t and CSI at time t + 1
-    mu, sigma = norm.fit(increments)
-
-    q_inc = qb
-    dstar = 3 * sigma
-    q_amp = math.ceil(math.log2(1 / dstar * (2 ** q_inc + 1)))  # quantize amplitudes over 2^q_amp levels
-
-    df_quant = quantize(df, 0, 2 ** q_amp - 1)  # quantize data over 2^qb levels
-    incr_quant = quantize_norm(increments, sigma, qb, mu=0, path=path)  # quantize increments over 2^qb levels
-
-    # fit normal distribution to increments to compute mean and sigma of the distribution
-    with open(os.path.join(path, MU_SIGMA_TXT), "w") as f:
-        f.write("MU: " + str(mu) + "\tSIGMA: " + str(sigma) + "\n")
-
-    save_mean_csi(df, os.path.join(path, MEAN_CSI_CSV))
-    return df_quant, incr_quant, q_inc, q_amp
+# def normpdf(x, mean, sd):
+#     var = float(sd) ** 2
+#     denom = (2 * math.pi * var) ** .5
+#     num = math.exp(-(float(x) - float(mean)) ** 2 / (2 * var))
+#     return num / denom
 
 
-def quantize_norm(increments, sigma, qb, mu=0, path=''):
-    dstar = 3 * sigma
+def mutual_info(mean_csi, csi, q_inc, q_ampl, problist):
+    # conditional probability of P(A|A_mean) is computed as the probability of drawing the increment delta_t from the
+    # gaussian distribution of the increments and obtaining A by adding delta_t to A_mean
+    pcsi_given_mean_csi = Decimal(1)
+    for i in range(len(csi)):
+        val = Decimal(csi[i] - mean_csi.iloc[0][i])
+        if val < -2 ** (q_inc - 1) + 1 or val >= 2 ** (
+                q_inc - 1):  # TODO: all CSIs satisfy this condition at some point
+            # pcsi_given_mean_csi *= Decimal(10 ** -10)
+            pcsi_given_mean_csi = Decimal(0)
+            break
+        # else:
+        pcsi_given_mean_csi *= Decimal(problist[int(val)])
 
-    sample = np.vectorize(lambda x: -dstar if x < -dstar else dstar if x > dstar else x)(
-        np.random.normal(loc=mu, scale=sigma, size=increments.size))
-    sample2 = np.vectorize(lambda x: -dstar if x < -dstar else dstar if x > dstar else x)(increments.values.flatten())
+    # pcsi
+    pcsi = Decimal(1 / (2 ** (
+            Decimal(q_ampl) * Decimal(len(csi)))))  # TODO: len(csi) == 242, this is always going to be equal to 0
+    # pmeancsi
+    pmean = Decimal(1 / (2 ** (
+            Decimal(q_ampl) * Decimal(len(csi)))))  # TODO: len(csi) == 242, this is always going to be equal to 0
 
-    snew = (sample - min(sample)) / (max(sample) - min(sample)) * (2 ** qb - 2) - (2 ** (qb - 1) - 1)
-    snew2 = (sample2 - min(sample2)) / (max(sample2) - min(sample2)) * (2 ** qb - 2) - (2 ** (qb - 1) - 1)
-
-    qsamples = [int(round(i)) for i in snew]
-    qsamples2 = [int(round(i)) for i in snew2]
-
-    # make sure that the distribution integrates to 1
-    occ = [qsamples.count(i) / len(qsamples) for i in range(-2 ** (qb - 1) + 1, 2 ** (qb - 1) + 1)]
-    print(sum(occ))
-
-    hist_dist_compared(path, qb, qsamples, qsamples2)
-    plot_dist_compared(path, qb, qsamples, qsamples2)
-
-    return qsamples
-
-
-def hist_dist_compared(path, qb, qsamples, qsamples2):
-    y, x = np.histogram(qsamples, bins=range(- 2 ** (qb - 1) + 1, 2 ** (qb - 1) + 1))
-    y = y / sum(y)  # normalize histogram
-    y2, x = np.histogram(qsamples2, bins=range(- 2 ** (qb - 1) + 1, 2 ** (qb - 1) + 1))
-    y2 = y2 / sum(y2)  # normalize histogram
-    plt.bar(x[:-1], y, align='center', label='Sample')
-    plt.bar(x[:-1], y2, align='center', label=str(qb) + ' bits', width=0.5)
-    plt.gca().set_xticks(x[:-1])
-    plt.yscale('log')
-    plt.legend()
-    plt.grid()
-    # plt.show()
-    plt.savefig(os.path.join(path, "hist_quant_incr_VS_sample_" + str(qb) + ".pdf"))
-    plt.close()
+    # logapprox = 2 * ((pcsi_given_mean_csi - pcsi) / (pcsi_given_mean_csi + pcsi))
+    # return pcsi_given_mean_csi * pmean * Decimal.ln(pcsi_given_mean_csi) - pcsi_given_mean_csi * pcsi * Decimal.ln(
+    #         pcsi) if pcsi_given_mean_csi != 0 else 0
+    # logapprox = pcsi_given_mean_csi / pcsi - 1
+    return pcsi_given_mean_csi * (pcsi_given_mean_csi - pmean)
 
 
-def plot_dist_compared(path, qb, qsamples, qsamples2):
-    y, x = np.histogram(qsamples, bins=range(- 2 ** (qb - 1) + 1, 2 ** (qb - 1) + 1))
-    y = y / sum(y)  # normalize histogram
-    y2, x = np.histogram(qsamples2, bins=range(- 2 ** (qb - 1) + 1, 2 ** (qb - 1) + 1))
-    y2 = y2 / sum(y2)  # normalize histogram
-    plt.plot(x[:-1], y, label='Sample')
-    plt.plot(x[:-1], y2, label=str(qb) + ' bits')
-    plt.gca().set_xticks(x[:-1])
-    plt.yscale('log')
-    plt.legend()
-    plt.grid()
-    # plt.show()
-    plt.savefig(os.path.join(path, "plot_quant_incr_VS_sample_" + str(qb) + ".pdf"))
-    plt.close()
-
-
-def save_mean_csi(df, path):
-    """Save mean CSI values to a CSV file.
-    :param df: dataframe to compute the mean CSI on
-    :param path: path to save the CSV file
+def int_mi(df_quant_A, mean_csi_A, q_inc, q_ampl, problist):
     """
-    if not path.endswith(".csv"):  # fix file extension
-        path = path[:path.rfind(".")]
-        path += ".csv"
-    with open(path, "w") as mcsi:
-        for col in df.columns:
-            mcsi.write(str(df[col].mean()) + ",")
-        mcsi.truncate(mcsi.tell() - 1)  # remove last comma
-    mcsi.close()
-
-
-def normalize(df):
-    """Normalize data so that it ranges from 0 to 1.
-    :param df: dataframe to normalize
-    :return: normalized dataframe
+    Compute the mutual information between each row of the input dataframe (CSI) and a reference CSI of the same df
+    :param mean_csi_A: reference CSI
+    :param df_quant_A: input dataframe
+    :param q_inc: number of bits used to quantize the incrments
+    :param q_ampl: number of bits used to quantize the amplitude
+    :param problist: list of probabilities of extracting an increment from the quantized distribution
+    :return: internal mutual information
     """
-    df = df - df.min().min()  # shift data so that it ranges from 0 to max
-    df = df / (df.max().max())  # normalize data so that it ranges from 0 to 1
-    return df
+    int_info = 0
+    for i in range(len(df_quant_A) - 1):
+        int_info += mutual_info(mean_csi_A, df_quant_A.iloc[i], q_inc, q_ampl, problist)
+    return int_info
 
-
-def quantize(df, a=0, b=255):
-    """Quantize data into an integer number of levels ranging from a to b.
-    :param df: dataframe to quantize
-    :param a: lower bound of the quantization interval
-    :param b: upper bound of the quantization interval
-    :return: quantized dataframe
-    """
-    return round(df * (b - a) + a)
+# def ext_mi(df_quant_A, mean_csi_A, df_quant_B, mean_csi_B):
+#     """
+#     Compute the mutual information between each row of the input df (CSI) and a reference CSI from a different df
+#     :param df_quant_A: input dataframe representing experiment A
+#     :param mean_csi_A: reference CSI of experiment A
+#     :param df_quant_B: input dataframe representing experiment B
+#     :param mean_csi_B: reference CSI of experiment B
+#     :return: external mutual information I(mean_A, B) and I(mean_B, A)
+#     """
+#     ext_info_AB = 0
+#     for i in range(len(df_quant_B)):
+#         ext_info_AB += mutual_info(mean_csi_A, df_quant_B.iloc[i])
+#
+#     ext_info_BA = 0
+#     for i in range(len(df_quant_A)):
+#         ext_info_BA += mutual_info(mean_csi_B, df_quant_A.iloc[i])
+#     return ext_info_AB, ext_info_BA
