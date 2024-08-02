fixed full whd computation. save data on json file
Signed-off-by: Elena Tonini <e.tonini002@studenti.unibs.it>

main.py
@@ -9,9 +9,10 @@ import quant
 from histograms import plot_histogram_for_sc
 from increments import plot_increments_for_sc
 from plotcsi import plotcsi_quant
+from plotwhd import plt_superimposed_whd
 from setup import print_menu, set_params, load_data, removeext
 from time_evolution import plot_time_evolution_for_sc
-from whd import whd_matrix
+from whd import full_whd_matrix
 
 np.random.seed(seed=527302)
 
@@ -83,32 +84,27 @@ if __name__ == '__main__':
             dfqs[k][k1] = df_quant
             plotcsi_quant(df, df_quant, q_amp=q_amp, n=10, path=dst_folder)
 
-            bsc_processing(df, df_quant, dst_folder)
+            # bsc_processing(df, df_quant, dst_folder)
 
-        # COMPUTING MUTUAL INFORMATION
-        # problist = {}
-        # for i in range(-2 ** (q_inc - 1) + 1, 2 ** (q_inc - 1)):
-        #     problist[i] = art_incr_quant.count(i) / len(art_incr_quant)
-        # int_info = mi.int_mi(df_quant, mean_csi, q_inc, q_amp, problist)
+    # COMPUTING MUTUAL INFORMATION
+    # problist = {}
+    # for i in range(-2 ** (q_inc - 1) + 1, 2 ** (q_inc - 1)):
+    #     problist[i] = art_incr_quant.count(i) / len(art_incr_quant)
+    # int_info = mi.int_mi(df_quant, mean_csi, q_inc, q_amp, problist)
 
-        # COMPUTING WEIGHTED HAMMING DISTANCE
-        # whd = whd_int(df_quant, mean_csi2)
-        # dst_folder = os.path.join(os.getcwd(), k)
-        # whd_std, whd_mean = whd_matrix(dfs=dfs[k],  # passing dfs relative to a single experiment
-        #                                workdir=k,  # name of the folder containing the data
-        #                                dfqs=dfqs,  # dictionary containing the quantized data
-        #                                nsc=num_sc,  # number of subcarriers
-        #                                q_amp=q_amp,  # quantization level
-        #                                stddevpath=dst_folder, meanpath=dst_folder)  # path where to save the output
-        dst_folder = os.path.join(os.getcwd(), k)
-        x_whd_std, x_whd_mean = whd_matrix(dfs=dfs[k],  # passing dfs relative to a single experiment
-                                           workdir=k,  # name of the folder containing the data
-                                           dfqs=dfqs,  # dictionary containing the quantized data
-                                           nsc=num_sc,  # number of subcarriers
-                                           q_amp=q_amp,  # quantization level
-                                           stddevpath=dst_folder, meanpath=dst_folder)  # path where to save the output
+    # COMPUTING WEIGHTED HAMMING DISTANCE
+    # whd = whd_int(df_quant, mean_csi2)
+    dst_folder = os.path.join(os.getcwd(), "out")
+    if not os.path.exists(dst_folder):
+        os.makedirs(dst_folder)
+    whd_std, whd_mean = full_whd_matrix(dfs=dfs,  # passing dfs relative to a single experiment
+                                        dfqs=dfqs,  # dictionary containing the quantized data
+                                        nsc=num_sc,  # number of subcarriers
+                                        q_amp=q_amp,  # quantization level
+                                        stddevpath=dst_folder, meanpath=dst_folder)  # path where to save the output
 
     # PLOTTING
-    # plt_superimposed_whd(df_quant, mean_csi, df_quant2, mean_csi2, df_quant3, mean_csi3, dst_folder)
+    dfq_plot = []  # TODO: add dataframes to plot
+    plt_superimposed_whd(dfqs, dst_folder)
     # plt_whd_boxplot(df_quant, mean_csi, df_quant2, mean_csi2, df_quant3, mean_csi3, dst_folder)
     # plt_whd_violin(df_quant, mean_csi, df_quant2, mean_csi2, df_quant3, mean_csi3, dst_folder)

whd.py
@@ -1,10 +1,9 @@
-import csv
+import json
 import os
 
 import numpy as np
 import pandas as pd
 
-import setup
 from quant import quantize, mean_csi_comp
 
 MTXSTDDEV = "whd_std.csv"
@@ -26,57 +25,75 @@ def whd(df, ref):
     return dists
 
 
-def whd_matrix(dfs, workdir, dfqs, nsc, q_amp=8, stddevpath=None, meanpath=None):
+def whd_matrix(dfs, workdir, dfqs, nsc, q_amp=9, stddevpath=None, meanpath=None):
+    # if used, pass the dfs[experiment] dictionary, not dfs as a whole.
+    # in main it was used as (in the "for k1, df in dframes.items()" loop):
+    # whd_std, whd_mean = whd_matrix(dfs=dfs[k],  # passing dfs relative to a single experiment
+    #                                workdir=k,  # name of the folder containing the data
+    #                                dfqs=dfqs,  # dictionary containing the quantized data
+    #                                nsc=num_sc,  # number of subcarriers
+    #                                q_amp=q_amp,  # quantization level
+    #                                stddevpath=dst_folder, meanpath=dst_folder)  # path where to save the output
+
     if len(dfs) == 0:
         return [[0]], [[0]]
-
-    m_stddev = [[0 for _ in range(len(dfs))] for _ in range(len(dfs))]
-    m_mean = [[0 for _ in range(len(dfs))] for _ in range(len(dfs))]
-
     normfact = nsc * ((2 ** q_amp) - 1)
 
-    i = 0
-    for k, v in dfs.items():
-        mean_csi = quantize(mean_csi_comp(dfs[k]), 0, 2 ** q_amp - 1)
-        j = 0
-        for k1, v1 in dfs.items():
-            d = whd(dfqs[workdir][k1], mean_csi)
+    m_stddev = {}
+    m_mean = {}
+    for k, v in dfs.items():  # for each capture k in the experiment
+        mean_csi = quantize(mean_csi_comp(dfs[k]), 0, 2 ** q_amp - 1)  # quantize the mean CSI of k
+        m_stddev[k] = {}
+        m_mean[k] = {}
+        for k1, v1 in dfs.items():  # for each capture k1 in the experiment
+            d = whd(dfqs[workdir][k1], mean_csi)  # compute the WHD between the mean CSI of k and the CSIs of k1
             l = []
             for e in d.values():
                 l.append(e)
-            m_stddev[i][j] = np.std(l) / normfact
-            m_mean[i][j] = np.mean(l) / normfact
-            j += 1
-        i += 1
+            m_stddev[k][k1] = np.std(l) / normfact
+            m_mean[k][k1] = np.mean(l) / normfact
 
         if stddevpath is not None:
-            save_whd(os.path.join(stddevpath, setup.removeext(k)), MTXSTDDEV, m_stddev)
+            whd_to_json(m_stddev, os.path.join(stddevpath, MTXSTDDEV))
         if meanpath is not None:
-            save_whd(os.path.join(meanpath, setup.removeext(k)), MTXMEAN, m_mean)
-
+            whd_to_json(m_mean, os.path.join(meanpath, MTXMEAN))
     return m_stddev, m_mean
 
 
-def ext_whd_matrix(dfqs, nsc, q_amp=8, stddevpath=None, meanpath=None):
-    if len(dfqs) == 0:
+def whd_to_json(data, path):
+    with open(path, 'w') as fp:
+        json.dump(data, fp)
+
+
+def full_whd_matrix(dfs, dfqs, nsc, q_amp=9, stddevpath=None, meanpath=None):
+    if len(dfs) == 0:
         return [[0]], [[0]]
 
-    m_stddev = [[0 for _ in range(len(dfqs[i]))] for i in range(len(dfqs))]
-    m_mean = [[0 for _ in range(len(dfqs[i]))] for i in range(len(dfqs))]
+    ncap = 0
+    for k, v in dfs.items():
+        ncap += len(v)
     normfact = nsc * ((2 ** q_amp) - 1)
 
-    for k in range(len(dfqs)):  # for each folder of data
-        pass
+    m_stddev = {}
+    m_mean = {}
+    for k, v in dfs.items():  # for each folder
+        m_stddev[k] = {}
+        m_mean[k] = {}
+        for k1, v1 in dfs[k].items():  # for each capture
+            mean_csi = quantize(mean_csi_comp(dfs[k][k1]), 0, 2 ** q_amp - 1)  # quantize the mean CSI of capture k1
+            m_stddev[k][k1] = {}
+            m_mean[k][k1] = {}
+            for k2, v2 in dfs.items():  # for each folder
+                for k3, v3 in dfs[k2].items():  # for each capture
+                    d = whd(dfqs[k2][k3], mean_csi)  # compute the WHD between the mean CSI of k1 and the CSIs of k3
+                    l = []
+                    for e in d.values():
+                        l.append(e)
+                    m_stddev[k][k1][k3] = np.std(l) / normfact  # folder, capture1, capture2
+                    m_mean[k][k1][k3] = np.mean(l) / normfact
+
     if stddevpath is not None:
-        save_whd(stddevpath, MTXSTDDEV, m_stddev)
+        whd_to_json(m_stddev, os.path.join(stddevpath, MTXSTDDEV))
     if meanpath is not None:
-        save_whd(meanpath, MTXMEAN, m_mean)
-
+        whd_to_json(m_mean, os.path.join(meanpath, MTXMEAN))
     return m_stddev, m_mean
-
-
-def save_whd(path, outfile, idata):
-    global f, wr
-    with open(os.path.join(path, outfile), "w") as f:
-        wr = csv.writer(f, delimiter=',', lineterminator='\n')
-        wr.writerows(idata)