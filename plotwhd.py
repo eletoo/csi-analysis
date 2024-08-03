380bd3b684f2a17e699ceeb24b959528fbf3d0f7
Elena Tonini <e.tonini002@studenti.unibs.it>
2024-08-03 18:54:58 +0200

    corrected plot of distances
    Signed-off-by: Elena Tonini <e.tonini002@studenti.unibs.it>

--- plotwhd.py
+++ plotwhd.py
@@ -3,19 +3,19 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
-import quant
+from quant import quantize, mean_csi_comp
 from whd import whd
 
 
-def plt_superimposed_whd(df_quant, dst_folder, labels=None):
+def plt_superimposed_whd(df_quant, df, q_amp, dst_folder, labels=None):
     if labels is None:
         labels = []
     plt.title("Distribution of WHD values")
-    i = 0
-    for v in df_quant:
-        hist, edges = np.histogram(list(whd(v, quant.mean_csi_comp(v)).values()), bins=100)
+    for i in range(len(df_quant)):
+        hist, edges = np.histogram(
+            list(whd(df_quant[i], quantize(mean_csi_comp(df[i]), 0, 2 ** q_amp - 1)).values()), bins=100)
         plt.plot(edges[:-1], hist, label=labels[i] if i < len(labels) else "#ppl: " + str(i))
-        i += 1
+
     plt.grid()
     plt.xlabel(r'$WHD(A_c, A_c^*)$')
     plt.ylabel('Frequency')
