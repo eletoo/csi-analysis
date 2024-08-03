fixed bug in computation of whd
Signed-off-by: Elena Tonini <e.tonini002@studenti.unibs.it>

plotcsi.py
@@ -23,7 +23,8 @@ def plotcsi(df, n=1, title='CSI Amplitude', xlabel='Subcarrier index', ylabel='A
     plt.close()
 
 
-def plotcsi_quant(df, df_quant, n=1, q_amp=8, title='CSI Amplitude', xlabel=r'Subcarrier index ($n$)', ylabel=r'$A_c$',
+def plotcsi_quant(dfnorm, df_quant, n=1, q_amp=8, title='CSI Amplitude', xlabel=r'Subcarrier index ($n$)',
+                  ylabel=r'$A_c$',
                   path=''):
     # undo quantization
     a = 0
@@ -31,12 +32,10 @@ def plotcsi_quant(df, df_quant, n=1, q_amp=8, title='CSI Amplitude', xlabel=r'Su
     dequant = (df_quant - a) * (b - a)
     dequant = quant.normalize(dequant)
 
-    dfnorm = quant.normalize(df)
-
     # plt.figure()
     fig, ax = plt.subplots(3, 1, sharex=True)
     for i in range(n):
-        idx = np.random.randint(0, len(df))
+        idx = np.random.randint(0, len(dfnorm))
         ax[0].plot(dfnorm.iloc[idx], label=r'$A({' + str(idx) + '},n)$')
         ax[1].plot(df_quant.iloc[idx], label=r'$A({' + str(idx) + '},n)^q$')
         ax[2].plot(dfnorm.iloc[idx] - dequant.iloc[idx], label=r'$A({' + str(idx) + '},n)-A({' + str(idx) + '},n)^q$')
@@ -45,7 +44,7 @@ def plotcsi_quant(df, df_quant, n=1, q_amp=8, title='CSI Amplitude', xlabel=r'Su
     fig.suptitle(title)
 
     plt.xlabel(xlabel)
-    xticks(np.arange(0, len(df.columns), 25), fontsize=6)
+    xticks(np.arange(0, len(dfnorm.columns), 25), fontsize=6)
 
     ax[0].set_ylim(0, 1)
     ax[0].set_ylabel(ylabel)

quant.py
@@ -3,7 +3,6 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from scipy.stats import norm
 
 MEAN_CSI_CSV = "mean_CSI.csv"
@@ -31,7 +30,7 @@ def quant(df, qb=QB, path=""):
     save_mean_csi(df, os.path.join(path, MEAN_CSI_CSV))
     mean_csi = quantize(mean_csi_comp(df), 0, 2 ** q_amp - 1)
 
-    return df_quant, art_incrq, incrq, q_inc, q_amp, mean_csi, sigma
+    return df, df_quant, art_incrq, incrq, q_inc, q_amp, mean_csi, sigma
 
 
 def quantize_incr(df, path, qb):
@@ -56,7 +55,7 @@ def quantize_norm(increments, sigma, qb, mu=0, path=''):
 
     # make sure that the distribution integrates to 1
     occ = [qsamples.count(i) / len(qsamples) for i in range(-2 ** (qb - 1) + 1, 2 ** (qb - 1) + 1)]
-    print(sum(occ))
+    print("\n" + str(sum(occ)) + "\n")
 
     hist_dist_compared(path, qb, qsamples, qsamples2)
 
@@ -87,7 +86,7 @@ def mean_csi_comp(df):
     """Compute mean CSI values.
     :param df: dataframe to compute the mean CSI on
     """
-    return df.mean()
+    return df.mean(axis=0)
 
 
 def save_mean_csi(df, path):